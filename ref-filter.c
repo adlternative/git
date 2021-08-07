39d3b35fe35b61c7dd659448688a1b40fc463f42
ZheNing Hu <adlternative@gmail.com>
2021-08-07 13:44:10 +0800

    [GSOC] ref-filter: reduce scan object buffer
    Test                                                HEAD              this tree
    -------------------------------------------------------------------------------------------
    1006.2: cat-file --batch-check                      0.11(0.09+0.02)   0.11(0.10+0.01) +0.0%
    1006.3: cat-file --batch-check with atoms           0.09(0.08+0.00)   0.09(0.06+0.02) +0.0%
    1006.4: cat-file --batch-check with contents atom   0.57(0.54+0.02)   0.56(0.52+0.04) -1.8%
    1006.5: cat-file --batch-check with person atoms    0.55(0.53+0.02)   0.55(0.53+0.02) +0.0%
    1006.6: cat-file --batch                            0.58(0.56+0.01)   0.58(0.53+0.04) +0.0%
    1006.7: cat-file --batch with atoms                 0.56(0.54+0.02)   0.55(0.51+0.03) -1.8%
    1006.8: cat-file --batch with person atoms          0.57(0.54+0.02)   0.57(0.52+0.04) +0.0%
    1006.9: cat-file --batch with person atoms          0.63(0.62+0.01)   0.62(0.59+0.03) -1.6%
    
    Mentored-by: Christian Couder <christian.couder@gmail.com>
    Mentored-by: Hariom Verma <hariom18599@gmail.com>
    Signed-off-by: ZheNing Hu <adlternative@gmail.com>

--- ref-filter.c
+++ ref-filter.c
@@ -1186,69 +1186,30 @@ static void grab_commit_values(struct atom_value *val, int deref, struct object 
 	}
 }
 
-static const char *find_wholine(const char *who, int wholen, const char *buf)
+static const char *copy_email(const char *buf, const char *end, struct used_atom *atom)
 {
-	const char *eol;
-	while (*buf) {
-		if (!strncmp(buf, who, wholen) &&
-		    buf[wholen] == ' ')
-			return buf + wholen + 1;
-		eol = strchr(buf, '\n');
-		if (!eol)
-			return "";
-		eol++;
-		if (*eol == '\n')
-			return ""; /* end of header */
-		buf = eol;
-	}
-	return "";
-}
-
-static const char *copy_line(const char *buf)
-{
-	const char *eol = strchrnul(buf, '\n');
-	return xmemdupz(buf, eol - buf);
-}
-
-static const char *copy_name(const char *buf)
-{
-	const char *cp;
-	for (cp = buf; *cp && *cp != '\n'; cp++) {
-		if (!strncmp(cp, " <", 2))
-			return xmemdupz(buf, cp - buf);
-	}
-	return xstrdup("");
-}
-
-static const char *copy_email(const char *buf, struct used_atom *atom)
-{
-	const char *email = strchr(buf, '<');
-	const char *eoemail;
-	if (!email)
+	if (!buf)
 		return xstrdup("");
 	switch (atom->u.email_option.option) {
 	case EO_RAW:
-		eoemail = strchr(email, '>');
-		if (eoemail)
-			eoemail++;
 		break;
 	case EO_TRIM:
-		email++;
-		eoemail = strchr(email, '>');
+		buf++;
+		end = strchr(buf, '>');
 		break;
 	case EO_LOCALPART:
-		email++;
-		eoemail = strchr(email, '@');
-		if (!eoemail)
-			eoemail = strchr(email, '>');
+		buf++;
+		end = strchr(buf, '@');
+		if (!end)
+			end = strchr(buf, '>');
 		break;
 	default:
 		BUG("unknown email option");
 	}
 
-	if (!eoemail)
+	if (!end)
 		return xstrdup("");
-	return xmemdupz(email, eoemail - email);
+	return xmemdupz(buf, end - buf);
 }
 
 static char *copy_subject(const char *buf, unsigned long len)
@@ -1268,10 +1229,8 @@ static char *copy_subject(const char *buf, unsigned long len)
 	return strbuf_detach(&sb, NULL);
 }
 
-static void grab_date(const char *buf, struct atom_value *v, const char *atomname)
+static void grab_date(struct ident_split *ident, struct atom_value *v, const char *atomname)
 {
-	const char *eoemail = strstr(buf, "> ");
-	char *zone;
 	timestamp_t timestamp;
 	long tz;
 	struct date_mode date_mode = { DATE_NORMAL };
@@ -1289,12 +1248,10 @@ static void grab_date(const char *buf, struct atom_value *v, const char *atomnam
 		parse_date_format(formatp, &date_mode);
 	}
 
-	if (!eoemail)
-		goto bad;
-	timestamp = parse_timestamp(eoemail + 2, &zone, 10);
+	timestamp = parse_timestamp(ident->date_begin, NULL, 10);
 	if (timestamp == TIME_MAX)
 		goto bad;
-	tz = strtol(zone, NULL, 10);
+	tz = strtol(ident->tz_begin, NULL, 10);
 	if ((tz == LONG_MIN || tz == LONG_MAX) && errno == ERANGE)
 		goto bad;
 	v->s = xstrdup(show_date(timestamp, tz, &date_mode));
@@ -1306,12 +1263,13 @@ static void grab_date(const char *buf, struct atom_value *v, const char *atomnam
 }
 
 /* See grab_values */
-static void grab_person(enum atom_type type, struct atom_value *val, int deref, void *buf)
+static const void* grab_person(enum atom_type type, struct atom_value *val, int deref, const void *buf)
 {
 	int i;
 	const char *who = valid_atom[type].name;
-	int wholen = strlen(who);
 	const char *wholine = NULL;
+	struct ident_split ident;
+	size_t ident_len;
 
 	for (i = 0; i < used_atom_cnt; i++) {
 		const char *name = used_atom[i].name;
@@ -1331,18 +1289,20 @@ static void grab_person(enum atom_type type, struct atom_value *val, int deref, 
 		      type != ATOM_TAGGER && type != ATOM_COMMITTER)))
 			continue;
 		if (!wholine)
-			wholine = find_wholine(who, wholen, buf);
-		if (!wholine)
-			return; /* no point looking for it */
+			wholine = find_commit_header(buf, who, &ident_len);
+		if (!wholine || split_ident_line(&ident, wholine, ident_len))
+			return NULL; /* no point looking for it */
 		if (atom_type == type || atom_type == ATOM_CREATOR)
-			v->s = copy_line(wholine);
+			v->s = xmemdupz(wholine, ident_len);
 		else if (atom_type == type + 1)
-			v->s = copy_name(wholine);
+			v->s = xmemdupz(ident.name_begin, ident.name_end - ident.name_begin);
 		else if (atom_type == type + 2)
-			v->s = copy_email(wholine, &used_atom[i]);
-		else if (atom_type == type + 3 || atom_type == ATOM_CREATORDATE)
-			grab_date(wholine, v, name);
+			v->s = copy_email(ident.mail_begin - 1, ident.mail_end + 1, &used_atom[i]);
+		else if ((atom_type == type + 3 || atom_type == ATOM_CREATORDATE) &&
+			  ident.date_begin && ident.tz_begin)
+			grab_date(&ident, v, name);
 	}
+	return wholine ? wholine + ident_len + 1 : NULL;
 }
 
 static void find_subpos(const char *buf,
@@ -1423,7 +1383,7 @@ static void append_lines(struct strbuf *out, const char *buf, unsigned long size
 }
 
 /* See grab_values */
-static void grab_sub_body_contents(struct atom_value *val, int deref, struct expand_data *data)
+static void grab_sub_body_contents(struct atom_value *val, int deref, struct expand_data *data, const char *start_buffer)
 {
 	int i;
 	const char *subpos = NULL, *bodypos = NULL, *sigpos = NULL;
@@ -1461,7 +1421,7 @@ static void grab_sub_body_contents(struct atom_value *val, int deref, struct exp
 		     !starts_with(name, "contents")))
 			continue;
 		if (!subpos)
-			find_subpos(buf,
+			find_subpos(start_buffer,
 				    &subpos, &sublen,
 				    &bodypos, &bodylen, &nonsiglen,
 				    &sigpos, &siglen);
@@ -1525,28 +1485,29 @@ static void fill_missing_values(struct atom_value *val)
 static void grab_values(struct atom_value *val, int deref, struct object *obj, struct expand_data *data)
 {
 	void *buf = data->content;
+	const char *next_line = NULL;
 
 	switch (data->type) {
 	case OBJ_TAG:
 		if (obj)
 			grab_tag_values(val, deref, obj);
-		grab_sub_body_contents(val, deref, data);
-		grab_person(ATOM_TAGGER, val, deref, buf);
+		next_line = grab_person(ATOM_TAGGER, val, deref, buf);
+		grab_sub_body_contents(val, deref, data, next_line ? next_line : buf);
 		break;
 	case OBJ_COMMIT:
 		if (obj)
 			grab_commit_values(val, deref, obj);
-		grab_sub_body_contents(val, deref, data);
-		grab_person(ATOM_AUTHOR, val, deref, buf);
-		grab_person(ATOM_COMMITTER, val, deref, buf);
+		next_line = grab_person(ATOM_AUTHOR, val, deref, buf);
+		next_line = grab_person(ATOM_COMMITTER, val, deref, next_line ? next_line : buf);
+		grab_sub_body_contents(val, deref, data, next_line ? next_line : buf);
 		break;
 	case OBJ_TREE:
 		/* grab_tree_values(val, deref, obj, buf, sz); */
-		grab_sub_body_contents(val, deref, data);
+		grab_sub_body_contents(val, deref, data, buf);
 		break;
 	case OBJ_BLOB:
 		/* grab_blob_values(val, deref, obj, buf, sz); */
-		grab_sub_body_contents(val, deref, data);
+		grab_sub_body_contents(val, deref, data, buf);
 		break;
 	default:
 		die("Eh?  Object of type %d?", obj->type);
