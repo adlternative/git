663fded74dc64428be9c280a03279d73d426fb6e
ZheNing Hu <adlternative@gmail.com>
2021-08-12 15:00:13 +0800

    [GSOC] ref-filter: remove second parsing in format_ref_array_item
    We parsed format string in verify_ref_format() and stored the parsed
    atom in used_atom array. But in format_ref_array_item() we have
    another round of parsing format string. This affects performance.
    
    Introducing the struct parsed_atom_list which can save the current
    atom's start and end address in format string and its index in
    used_atom. All parsed_atom_list entry are linked together in the
    form of linked list, and the head node of the linked list is stored
    in struct ref_format. We can use clear_parsed_atom_list() to clear
    the nodes on the linked list.
    
    This can bring 1.9% performance improvement for git cat-file --batch.
    
    Mentored-by: Christian Couder <christian.couder@gmail.com>
    Mentored-by: Hariom Verma <hariom18599@gmail.com>
    Signed-off-by: ZheNing Hu <adlternative@gmail.com>

--- ref-filter.h
+++ ref-filter.h
@@ -71,6 +71,13 @@ struct ref_filter {
 		verbose;
 };
 
+struct parsed_atom_list {
+	const char *beg;
+	const char *end;
+	int at;
+	struct list_head list;
+};
+
 struct ref_format {
 	/*
 	 * Set these to define the format; make sure you call
@@ -84,6 +91,7 @@ struct ref_format {
 
 	/* Internal state to ref-filter */
 	int need_color_reset_at_eol;
+	struct list_head parsed_atom_head;
 };
 
 #define REF_FORMAT_INIT { .use_color = -1 }
@@ -111,6 +119,13 @@ struct ref_format {
 int filter_refs(struct ref_array *array, struct ref_filter *filter, unsigned int type);
 /*  Clear all memory allocated to ref_array */
 void ref_array_clear(struct ref_array *array);
+/* Free global memory allocated resource */
+void free_global_resource(void);
+/* Free ref_array_item's value */
+void free_ref_array_item_value(struct ref_array_item *item);
+/* Clear the parsed_atom_list in ref_format*/
+void clear_parsed_atom_list(struct list_head *parsed_atom_head);
+
 /*  Used to verify if the given format is correct and to parse out the used atoms */
 int verify_ref_format(struct ref_format *format);
 /*  Sort the given ref_array as per the ref_sorting provided */
